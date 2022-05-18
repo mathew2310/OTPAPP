0474b1222457eb72c9689c2eba61b8cc1c9ef5a9
Mathew Ijidakinro <matthewadetayo@gmail.com>
2022-05-18 18:14:22 +0100

    Initial Commit

---
+++ OTPApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTPApp
+//
+//  Created by Mathew Ijidakinro on 18/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
